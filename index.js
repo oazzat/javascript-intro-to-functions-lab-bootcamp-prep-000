56b835f0ea62edf437ef740cf92398e2e62b4db8
Omar Azzat <oazzat19@gmail.com>
2018-09-05 13:00:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
+function sayHiToGrandma(string){
+
+    if string === string.toLowerCase(){
+    return "I can\'t hear you!"
+    }
+    else {
+      return "YES INDEED"
+    }
+}
