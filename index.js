ad15b7d693c1450c758cf5b148e18ca4af7da5f4
Omar Azzat <oazzat19@gmail.com>
2018-09-05 13:04:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ function sayHiToGrandma(string){
     if (string === string.toLowerCase()){
     return "I can\'t hear you!"
     }
+    
+    else if (string === "I love you, Grandma."){
+      return "I love you, too"
+    }
+    
     else {
       return "YES INDEED!"
     }
